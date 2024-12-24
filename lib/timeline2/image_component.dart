e8439bc30741d20a87a1763b5192706a4daad3d1
safwen <safwen.baroumi@esprit.tn>
2024-12-24 19:52:49 +0100

    Correction des problèmes de formatage

--- lib/timeline2/image_component.dart
+++ lib/timeline2/image_component.dart
@@ -15,14 +15,14 @@ class ImageComponent extends StatefulWidget {
 
   const ImageComponent(
       {Key? key,
-        required this.imageSource,
-        this.isNetworkImage = true,
-        this.height = 100,
-        this.width = 100,
-        this.textSize = 12,
-        this.edgeInsets = const EdgeInsets.all(10),
-        this.isCircleImage = false,
-        this.isImageFromFile})
+      required this.imageSource,
+      this.isNetworkImage = true,
+      this.height = 100,
+      this.width = 100,
+      this.textSize = 12,
+      this.edgeInsets = const EdgeInsets.all(10),
+      this.isCircleImage = false,
+      this.isImageFromFile})
       : super(key: key);
 
   @override
@@ -36,46 +36,48 @@ class _ImageComponentState extends State<ImageComponent> {
       margin: widget.edgeInsets,
       child: widget.isNetworkImage
           ? ClipRRect(
-        borderRadius: widget.isCircleImage
-            ? BorderRadius.circular(100)
-            : BorderRadius.circular(0),
-        child: Image.network(
-          widget.imageSource!,
-          height: widget.height,
-          width: widget.width,
-          fit: BoxFit.cover,
-          errorBuilder: (context, error, stackTrace) =>
-              Text(error.toString(), style: TextStyle(fontSize: widget.textSize),),
-        ),
-      )
+              borderRadius: widget.isCircleImage
+                  ? BorderRadius.circular(100)
+                  : BorderRadius.circular(0),
+              child: Image.network(
+                widget.imageSource!,
+                height: widget.height,
+                width: widget.width,
+                fit: BoxFit.cover,
+                errorBuilder: (context, error, stackTrace) => Text(
+                  error.toString(),
+                  style: TextStyle(fontSize: widget.textSize),
+                ),
+              ),
+            )
           : widget.isImageFromFile != null
-          ? ClipRRect(
-          borderRadius: widget.isCircleImage
-              ? BorderRadius.circular(100)
-              : BorderRadius.circular(0),
-          child: kIsWeb
-              ? Image.memory(
-            widget.isImageFromFile.bytes,
-            width: widget.width,
-            height: widget.height,
-            fit: BoxFit.cover,
-          )
-              : Image.file(
-            File(widget.isImageFromFile!.path),
-            width: widget.width,
-            height: widget.height,
-            fit: BoxFit.cover,
-          ))
-          : ClipRRect(
-          borderRadius: widget.isCircleImage
-              ? BorderRadius.circular(100)
-              : BorderRadius.circular(0),
-          child: Image(
-            image: AssetImage(widget.imageSource!),
-            height: widget.height,
-            width: widget.width,
-            fit: BoxFit.cover,
-          )),
+              ? ClipRRect(
+                  borderRadius: widget.isCircleImage
+                      ? BorderRadius.circular(100)
+                      : BorderRadius.circular(0),
+                  child: kIsWeb
+                      ? Image.memory(
+                          widget.isImageFromFile.bytes,
+                          width: widget.width,
+                          height: widget.height,
+                          fit: BoxFit.cover,
+                        )
+                      : Image.file(
+                          File(widget.isImageFromFile!.path),
+                          width: widget.width,
+                          height: widget.height,
+                          fit: BoxFit.cover,
+                        ))
+              : ClipRRect(
+                  borderRadius: widget.isCircleImage
+                      ? BorderRadius.circular(100)
+                      : BorderRadius.circular(0),
+                  child: Image(
+                    image: AssetImage(widget.imageSource!),
+                    height: widget.height,
+                    width: widget.width,
+                    fit: BoxFit.cover,
+                  )),
     );
   }
 }
